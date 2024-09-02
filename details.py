d65da3d651c681f747b19b9f3915404005b76682
altafpathan65012 <137278745+altafpathan65012@users.noreply.github.com>
2024-09-02 17:38:26 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 1359658688566
-api_hash = "5f2f806877d356567g7ba957b7e5e1fc95a3e"
-bot_token = "6067884809764:AAGgyWLJCXEe5GpYN4HusxEWbCeE_uqVv3w"
-sudo_groups = -1001824573993
+api_id = 23031620
+api_hash = "31cb00c1cbe580394778b43105864bca"
+bot_token = "6865731231:AAEDdXn48VOzOf0jRo0CvxkU0VxIFsikTvg"
+sudo_groups = -1002155787742
