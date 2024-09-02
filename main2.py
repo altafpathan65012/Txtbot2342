653f82cf33f6e37cfb8bba295032f78587366eee
altafpathan65012 <137278745+altafpathan65012@users.noreply.github.com>
2024-09-02 17:43:50 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -39,11 +39,11 @@ bot = Client(
 
 #bot = Client(
 #    "bot",
-#    bot_token=os.environ.get("BOT_TOKEN"),
-#    api_id=int(os.environ.get("API_ID")),
-#    api_hash=os.environ.get("API_HASH")
+#    bot_token=os.environ.get("6865731231:AAEDdXn48VOzOf0jRo0CvxkU0VxIFsikTvg"),
+#    api_id=int(os.environ.get("23031620")),
+#    api_hash=os.environ.get("31cb00c1cbe580394778b43105864bca")
 #)
-#auth_users = [ int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != '']
+#auth_users = [ int(chat) for chat in os.environ.get("502980590").split(",") if chat != '']
 #sudo_users = auth_users
 #sudo_groups = [ int(chat) for chat in os.environ.get("GROUPS").split(",")  if chat != '']
 
